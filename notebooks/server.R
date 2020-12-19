3c3443e57674e83173d7c49d73b96deb6eb31219
anthonygertz <anthony.gertz@gmail.com>
2020-12-18 18:37:58 -0600

    After presentation 12/18/2020

--- notebooks/server.R
+++ notebooks/server.R
@@ -1,30 +1,190 @@
 shinyServer(function(input, output) {
+  #Age tab plots
+  output$plot_age <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata %>%
+               filter(location == input$location,
+                      statistic_group == 'Sex and Age', 
+                      estimate != pop_denom, 
+                      age_group != 'All') %>% 
+               group_by(location, statistic_group, age_group) %>%
+               summarise(pct_of_pop = sum(pct_of_pop)) %>%
+               ggplot(aes(x = age_group, y = pct_of_pop, fill = location))+
+               xlab("") + ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title=element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors)) %>%
+               layout(title = "Age by Generation")
+  })
+  
+  output$plot_age_m <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Sex and Age', 
+                      location == input$location, 
+                      estimate != pop_denom, 
+                      age_group != 'All', 
+                      sex == 'Male') %>%
+               group_by(location, statistic_group, age_group, sex) %>%
+               summarise(pct_of_pop = sum(pct_of_pop)) %>%
+               ggplot(aes(x = location, y = pct_of_pop, fill = age_group)) +
+               xlab("") + ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title=element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Age by Generation")
+  })
+  
+  
+  output$plot_age_f <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Sex and Age', 
+                      location == input$location, 
+                      estimate != pop_denom, 
+                      age_group != 'All', 
+                      sex == 'Female') %>%
+               group_by(location, statistic_group, age_group, sex) %>%
+               summarise(pct_of_pop = sum(pct_of_pop)) %>%
+               ggplot(aes(x = location, y = pct_of_pop, fill = age_group)) +
+               xlab("") + 
+               ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title = element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Age by Generation")
+            
+      
+  })
+  
+  #Education Tab Plots   
+  output$plot_edu <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Education', 
+                      location == input$location, 
+                      estimate != pop_denom, 
+                      statistic != 'Population 25+') %>%
+               group_by(location, statistic_group, statistic) %>%
+               #summarise(pct_of_pop = sum(pct_of_pop)) %>%
+               ggplot(aes(x = location, y = pct_of_pop, fill = statistic)) +
+               xlab("") + ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"),legend.title = element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Educational Attainment")
+  })
+  
+  output$plot_edu_m <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Education', 
+                      location == input$location, 
+                      estimate != pop_denom, 
+                      statistic != 'Population 25+', 
+                      sex == 'Male') %>%
+               group_by(location, statistic_group, statistic, sex) %>%
+               #summarise(pct_of_pop = sum(pct_of_pop)) %>%
+               ggplot(aes(x = location, y = pct_of_pop, fill = statistic)) +
+               xlab("") + ylab("% of Population") +
+               theme(
+                 plot.margin = unit(c(2,1,1,1), "cm"), legend.title=element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Educational Attainment")
+  })
+  
+  
+  output$plot_edu_f <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Education', 
+                      location == input$location, 
+                      estimate != pop_denom, 
+                      statistic != 'Population 25+', 
+                      sex == 'Female') %>%
+               group_by(location, statistic_group, statistic, sex) %>%
+               #summarise(pct_of_pop = sum(pct_of_pop)) %>%
+               ggplot(aes(x = location, y = pct_of_pop, fill = statistic)) +
+               xlab("") + 
+               ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title=element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Educational Attainment"
+             
+      )
+  })
+  
+  #Race tab plots  
+  output$plot_race <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Race', 
+                      location == input$location,
+                      estimate != pop_denom) %>%
+               ggplot(aes(y = pct_of_pop, x = location, fill = statistic)) +
+               xlab("") + 
+               ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title = element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Race")
     
-    output$mainplot <- renderPlot({
-        req(input$location)
-        censusdata %>%
-            filter(location == input$location) %>%
-                        filter(statistic_group == input$plot_choices) %>%
-            ggplot(aes(x = pct_of_pop, y = statistic, fill = location)) + 
-            geom_col(position = 'dodge') + 
-            facet_wrap(~location, ncol=1) + 
-            theme(legend.position = 'bottom')
-    })    
+  })
+ 
+  #Race tab plots  
+  output$plot_hisp <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Hispanic Identity', 
+                      location == input$location,
+                      estimate != pop_denom) %>%
+               ggplot(aes(y = pct_of_pop, x = location, fill = statistic)) +
+               xlab("") + 
+               ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title = element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Hispanic Identity") 
     
+  })
+  
+  output$plot_hisp_m <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Hispanic Identity', 
+                      location == input$location,
+                      estimate != pop_denom, 
+                      sex == 'Male') %>%
+               ggplot(aes(y = pct_of_pop, x = location, fill = statistic)) +
+               xlab("") + 
+               ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title = element_blank()) +
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Hispanic Identity") 
     
-
+  })
+  
+  output$plot_hisp_f <- renderPlotly({
+    req(input$location)
+    ggplotly(censusdata%>%
+               filter(statistic_group == 'Hispanic Identity', 
+                      location == input$location,
+                      estimate != pop_denom,
+                      sex == 'Female') %>%
+               ggplot(aes(y = pct_of_pop, x = location, fill = statistic)) +
+               xlab("") + 
+               ylab("% of Population") +
+               theme(plot.margin = unit(c(2,1,1,1), "cm"), legend.title = element_blank())+
+               geom_bar(position = 'stack', stat = 'identity') +
+               scale_fill_manual(values = custom_colors))%>%
+               layout(title = "Hispanic Identity")
     
-
+  })
+  
 })
 
 
-#Scatter Plots ??? Education vs. gender ???
-
-
-#Ensure Legend works
-#Set minimum widths???
-#Make sure file upload works??? integrating all of Maggi's code
-#Remove/filter out total populations
-#Special selections for "Age and Sex" and "Sex"
-#Shorten location names
 
--- notebooks/ui.R
+++ notebooks/ui.R
@@ -1,46 +1,52 @@
+header <- dashboardHeader(title = tags$a(href='https://people3.co',
+                                    tags$img(src='https://people3.co/wp-content/uploads/2020/06/People3-Logo-300.png', 
+                                    height='50',
+                                    width='200')))
 
-
-shinyUI(
-    dashboardPage(
-        
-        # Application title
-        dashboardHeader
-        (title = tags$a(href='https://people3.co',
-                        tags$img(src='https://people3.co/wp-content/uploads/2020/06/People3-Logo-300.png', 
-                                 height='50',width='200'))
-        ),
-        # Sidebar with a slider input for number of bins
-        dashboardSidebar(
-            checkboxGroupInput('location', 
-                               'Select Locations:', 
-                               choices = location_choices, 
-                               selected = NULL
-                               ), 
-            
-            radioButtons('plot_choices', 
-                               'Select a Category:', 
-                               choices = plot_choices, 
-            ),
+sidebar <- dashboardSidebar(sidebarMenu(
+              menuItem('Race & Identity', tabName = 'race_identity', icon = NULL),
+              menuItem('Hispanic Identity', tabName ='hisp_identity', icon = NULL),
+              menuItem('Age', tabName = 'age', icon = NULL),
+              menuItem('Education', tabName = 'education', icon = NULL),
+              checkboxGroupInput('location', 'Select One or More:', choices = location_choices, selected = NULL), 
          
+              fileInput("file", "Upload Your Company Profile:"),
             
-            fileInput("file", "Upload Your Company Profile:"),
+              actionButton('join', 'Join Uploaded File to Data', 
+                           icon = icon("cloud-upload-alt", class = NULL,
+                           lib = "font-awesome"))))
             
-            actionButton('join',
-                         'Join Uploaded File to Data', 
-                         icon = icon("cloud-upload-alt", class = NULL,
-                                     lib = "font-awesome"))
-            
-        ),
-        
-     
-        dashboardBody(people3_customTheme,
-                column(width = 8, 
-                       box(width = NULL),
-                       plotOutput('mainplot')
-                       )
-            
-                
-                      )
-        
-    )   
+body <- dashboardBody(tabItems(
+                      tabItem(tabName = 'race_identity', fluidRow(column(width = 12, tabBox(width = NULL, height = 700, 
+                                            tabPanel('Overview', plotlyOutput('plot_age')),
+                                            tabPanel('Male', plotlyOutput('plot_age_m')),
+                                            tabPanel('Female', plotlyOutput('plot_age_f'))),
+                                            
+                      ))),
+                      tabItem(tabName = 'hisp_identity', fluidRow(column(width = 12, tabBox(width = NULL, height = 700, 
+                                            tabPanel('Overview', plotlyOutput('plot_hisp')),
+                                            tabPanel('Male', plotlyOutput('plot_hisp_m')),
+                                            tabPanel('Female', plotlyOutput('plot_hisp_f'))),
+                                     
+                      ))),
+                      tabItem(tabName = 'age', fluidRow(column(width = 12, tabBox(width = NULL, height = 700, 
+                                            tabPanel('Overview', plotlyOutput('plot_age')),
+                                            tabPanel('Male', plotlyOutput('plot_age_m')),
+                                            tabPanel('Female',plotlyOutput('plot_age_f'))),
+                                
+                      ))),
+                      
+                      tabItem(tabName = 'education', fluidRow(column(width = 12, tabBox(width = NULL, height = 700, 
+                                            tabPanel('Overview', plotlyOutput('plot_edu')),
+                                            tabPanel('Male', plotlyOutput('plot_edu_m')),
+                                            tabPanel('Female', plotlyOutput('plot_edu_f'))),
+                                
+                      )))))
+       
+
+shinyUI(         
+dashboardPage(people3_customTheme,
+              header = header, 
+              sidebar = sidebar, 
+              body = body)
 )
